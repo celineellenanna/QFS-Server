464a6833cb0df1c6e285715aa5ce093ed72f8618
celineellenanna <celine.ott1@gmail.com>
2016-04-09 21:01:45 +0200

    started with quiz routes

---
+++ api/controllers/quiz.js
@@ -0,0 +1,36 @@
+var Log = require('../logs/index');
+var Quiz = require('../models/index').Quiz;
+
+var controller = {
+    // Index überhaupt benötigt?
+   /* index: function(req, res, next) {
+        Quiz.find(function(err, users) {
+            if(err) next(err);
+            if(users) {
+                res.send({ "success" : true, "message" : "User gefunden", data : users });
+            } else {
+                res.send({ "success" : false, "message" : "User nicht gefunden", data : null });
+            }
+        });
+    },*/
+    get: function(req, res, next) {
+        Quiz.findById(req.params.id, function(err, quiz) {
+            if(err) next(err);
+            if(quiz) {
+                res.send({ "success" : true, "message" : "Quiz gefunden", data : quiz });
+            } else {
+                res.send({ "success" : false, "message" : "Quiz nicht gefunden", data : null });
+            }
+        });
+    },
+    cancel: function(req, res, next) {
+        Quiz.findById(req.params.id, function(err, quiz) {
+            if(err) next(err);
+            quiz.status = 'Canceled';
+            quiz.save();
+            res.send({ "success" : true, "message" : "Quiz beendet", data : null });
+        });
+    }
+};
+
+module.exports = controller;
