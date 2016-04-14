ba12096cd53f4a726fe55b987187ae8f4aa1d162
celineott <cott@hsr.ch>
2016-04-14 14:33:09 +0200

    added new quiz route

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(express.static('./api/public'));
 app.use('/', indexRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/quiz', quizRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
