93d9f2116beaf015bb968035d75e50b99e711df4
celineott <cott@hsr.ch>
2016-05-17 13:18:37 +0200

    Refactoring

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/user');
 
-router.get('/', userController.index);
+router.get('/', userController.getAll);
 router.get('/:id', userController.get);
 router.get('/findOpponent/:id', userController.findOpponent);
 
