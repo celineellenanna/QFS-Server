1c3ba442344871b79ebec767d21ab8ae476cfd5d
steinhaeusl.patrick <steinhaeusl.patrick@gmail.com>
2016-05-27 16:24:58 +0200

    Update Config Files

--- api/configs/production.js
+++ api/configs/production.js
@@ -7,5 +7,5 @@ process.srv.port        = process.env.NODE_PORT || 443;
 //Database
 process.db              = {};
 process.db.host         = 'localhost';
-process.db.database     = 'qfs_dev';
+process.db.database     = 'qfs_prod';
 process.db.connection   = 'mongodb://' + process.db.host + '/' + process.db.database;
